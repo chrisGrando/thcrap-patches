ed38b1d3972fd884a7868fdee2836c9cf6925f1e
Christian Júlio de Liz Grando <47995783+chrisGrando@users.noreply.github.com>
2026-07-01 20:50:38 -0300

    Update supported games in Auto-Bomb patch

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "christianjulio2010@gmail.com",
 	"id": "kriChris",
 	"patches": {
-		"Auto-Bomb": "Automatically triggers a bomb as soon as you got hit. Supports Touhou 06, 07, 08, 10, 11, 12, 13, 14, 15, 16, 17, 18 and Uwabami Breakers.",
+		"Auto-Bomb": "Automatically triggers a bomb as soon as you got hit. Supports Touhou 06, 07, 08, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20 and Uwabami Breakers.",
 		"HitboxPatch": "EoSD always visible hitbox. Ported from Hitbox Patch v0.1",
 		"string-fix": "(TH07, TH08) Fix stage result text not showing. Please select this for LAST!"
 	},
